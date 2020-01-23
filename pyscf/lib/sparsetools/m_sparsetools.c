035c92580e869d663b484fab685571d82a9c57ae
marc barbry <marc.barbry@mailoo.org>
2020-01-23 11:33:37 +0100

    use dynamic schedule for omp for loop as done in eigen

--- pyscf/lib/sparsetools/m_sparsetools.c
+++ pyscf/lib/sparsetools/m_sparsetools.c
@@ -16,7 +16,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <omp.h>
-//#include <cblas.h>
+
 
 /*
 !
@@ -48,11 +48,14 @@ void scsr_matvec(int nrow, int ncol, int nnz, int *Ap, int *Aj,
   int i, jj;
   float sum = 0.0;
 
+  //printf("nthreads = %d, nrow = %d, dyn sche = %d\n", nthreads, nrow, (nrow + 4*nthreads-1)/(4*nthreads));
+
   # pragma omp parallel \
   shared (nrow, Yx, Ap, Ax, Xx, Aj) \
   private (i, jj, sum)
   {
-    #pragma omp for
+    int nthreads = omp_get_num_threads();
+    #pragma omp for schedule(dynamic, (nrow + 4*nthreads-1)/(4*nthreads))
     for(i = 0; i < nrow; i++){
       sum = Yx[i];
       for(jj = Ap[i]; jj < Ap[i+1]; jj++){
@@ -71,11 +74,15 @@ void dcsr_matvec(int nrow, int ncol, int nnz, int *Ap, int *Aj,
   int i, jj;
   double sum = 0.0;
 
+
   # pragma omp parallel \
   shared (nrow, Yx, Ap, Ax, Xx, Aj) \
   private (i, jj, sum)
   {
-    #pragma omp for
+    int nthreads = omp_get_num_threads();
+    //printf("nthreads = %d, nrow = %d, dyn sche = %d\n", nthreads, nrow, (nrow + 4*nthreads-1)/(4*nthreads));
+    //#pragma omp for schedule(dynamic)
+    #pragma omp for schedule(dynamic, (nrow + 4*nthreads-1)/(4*nthreads))
     for(i = 0; i < nrow; i++){
       sum = Yx[i];
       for(jj = Ap[i]; jj < Ap[i+1]; jj++){
