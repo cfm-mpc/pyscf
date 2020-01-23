a12bebaad3b3d358084a34b95095c1e79b6bc8a4
marc barbry <marc.barbry@mailoo.org>
2020-01-23 14:12:26 +0100

    parallel div_eigenergy_numba

--- pyscf/nao/m_div_eigenenergy_numba.py
+++ pyscf/nao/m_div_eigenenergy_numba.py
@@ -16,7 +16,7 @@ from __future__ import division
 import numba as nb
 import numpy as np
 
-@nb.jit(nopython=True)
+@nb.jit(nopython=True, parallel=True)
 def div_eigenenergy_numba(n2e, n2f, nfermi, vstart, comega, nm2v_re, nm2v_im):
     """
         multiply the temporary matrix by (fn - fm) (frac{1.0}{w - (Em-En) -1} -
@@ -28,7 +28,7 @@ def div_eigenenergy_numba(n2e, n2f, nfermi, vstart, comega, nm2v_re, nm2v_im):
     a0 = comega.real**2 - comega.imag**2
     b = 2*comega.real*comega.imag
     
-    for n in range(nfermi):
+    for n in nb.prange(nfermi):
         en = n2e[n]
         fn = n2f[n]
 
@@ -49,7 +49,7 @@ def div_eigenenergy_numba(n2e, n2f, nfermi, vstart, comega, nm2v_re, nm2v_im):
             nm2v_re[n, m] = nm2v_re_nm
             nm2v_im[n, m] = nm2v_im_nm
 
-    for n in range(vstart+1, nfermi):
+    for n in nb.prange(vstart+1, nfermi):
         for m in range(n-vstart):
             nm2v_re[n, m] = 0.0 
             nm2v_im[n, m] = 0.0
