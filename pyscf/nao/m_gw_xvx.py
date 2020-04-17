f718360449f0a58ee1ef701ca412530da3760324
marc barbry <marc.barbarosa@gmail.com>
2020-04-17 13:46:23 +0200

    custom mat mat avoiding allocation of temporary array in gw_xvx_dpcoo

--- pyscf/nao/m_gw_xvx.py
+++ pyscf/nao/m_gw_xvx.py
@@ -1,41 +1,104 @@
 from __future__ import division
 import numpy as np
+from scipy.sparse import coo_matrix, csr_matrix
+import numba as nb
+from memory_profiler import profile
 
+@profile
 def gw_xvx_dpcoo(self):
 
     xvx = []
     size = self.cc_da.shape[0]
     nfdp = self.pb.dpc2s[-1]
+    v_pd = self.v_dab.reshape(self.v_dab_csr.shape[0]*self.norbs, self.norbs).tocsr()
 
     for spin in range(self.nspin):
 
-        vxdp = dpcoo_step1(self, size, nfdp, spin)
-        vxdp = dpcoo_step2(vxdp, size, self.norbs)
-        vxdp = dpcoo_step3(self, vxdp, size, spin)
-        xvx.append(dpcoo_step4(self.cc_da, vxdp).todense())
+        xvx2 = csrmat_denmat_custom2(v_pd.indptr, v_pd.indices, v_pd.data,
+                                     self.mo_coeff[0, spin, :, :, 0].T,
+                                     self.mo_coeff[0, spin, self.nn[spin], :, 0],
+                                     nfdp, self.norbs, len(self.nn[spin]))
+        xvx2 = xvx2.reshape(len(self.nn[spin]), size, self.norbs)
+        xvx2 = np.swapaxes(xvx2, 1, 2)
+
+        xvx2_fin = np.zeros((xvx2.shape[0], xvx2.shape[1], self.cc_da.shape[1]),
+                             dtype=xvx2.dtype)
+        # Somehow dense.dot(sparse) uses a crazy amount of memory ...
+        # xvx2 = xvx2.dot(self.cc_da)
+        for i in range(xvx2_fin.shape[0]):
+            xvx2_fin[i, :, :] = self.cc_da_trans.dot(xvx2[i, :, :].T).T
+
+        xvx.append(xvx2_fin)
 
     return xvx
 
+@nb.jit(nopython=True)
+def csrmat_denmat_custom2(indptr, indices, data, B, X, N1, N2, N3):
+    """
+    Perform in one shot the following operations (avoiding the use of temporary arrays):
+
+    vxdp  = v_pd1.dot(xmb.T)
+    vxdp  = vxdp.reshape(size,self.norbs, self.norbs)
+    xvx2 = np.swapaxes(vxdp,0,1)
+    xvx2 = xvx2.reshape(self.norbs,-1)
+    xvx2 = xna.dot(xvx2)
+
+    The array vxdp is pretty larger: (size*norbs, norbs) and pretty dense (0.1%)
+    It is better to avoid its allocation for large systems.
+    """
+
+    D = np.zeros((N3, N1*N2), dtype=B.dtype)
+
+    for jp in range(N2):
+        Ctmp = np.zeros((N1*N2), dtype=B.dtype)
+        for ip in range(N1):
+            i = ip*N2 + jp
+            for kp in range(N2):
+                ipp = ip*N2 + kp
+
+                for ind in range(indptr[i], indptr[i+1]):
+                    k = indices[ind]
+                    Ctmp[ipp] += data[ind]*B[k, kp]
+
+        for ix in range(N3):
+            for jx in range(N1*N2):
+                D[ix, jx] += X[ix, jp]*Ctmp[jx]
+
+    return D
+
+@profile
 def dpcoo_step1(self, size, nfdp, spin):
     import sparse
 
-    xmb = sparse.COO.from_numpy(self.mo_coeff[0, spin, :, :, 0]).T
-    v_pd = sparse.COO.from_scipy_sparse(self.v_dab.reshape(nfdp*self.norbs, self.norbs))
+    xmb = csr_matrix(self.mo_coeff[0, spin, :, :, 0]).T
+    print("xmb: ", xmb.nnz, xmb.shape)
+    v_pd = self.v_dab_csr.reshape(nfdp*self.norbs, self.norbs)
+    vxdp = v_pd.dot(xmb).tocoo()
+    print("nnz: ", vxdp.nnz, "shape: ", vxdp.shape)
+    return sparse.COO.from_scipy_sparse(vxdp)
 
-    return sparse.COO.dot(v_pd, xmb)
-
+@profile
 def dpcoo_step2(vxdp, size, norbs):
 
-    return vxdp.reshape((size, norbs, \
-            norbs)).transpose(axes=(1, 0, 2)).reshape((norbs, size*norbs))
+    xxv2 = vxdp.reshape((size, norbs, norbs))
+    print("nnz: ", xxv2.nnz, "shape: ", xxv2.shape)
+    xxv2 = xxv2.transpose(axes=(1, 0, 2))
+    print("nnz: ", xxv2.nnz, "shape: ", xxv2.shape)
+    xxv2 = xxv2.reshape((norbs, size*norbs))
+    print("nnz: ", xxv2.nnz, "shape: ", xxv2.shape)
+    return xxv2
 
+@profile
 def dpcoo_step3(self, vxdp, size, spin):
     import sparse
 
     xna = sparse.COO.from_numpy(self.mo_coeff[0, spin, self.nn[spin], :, 0])
+    print("xna:", xna.shape, xna.nnz)
     xxv2 = xna.dot(vxdp).reshape((len(self.nn[spin]), size, self.norbs))
+    print("xxv2:", xxv2.shape, xxv2.nnz)
     return xxv2.transpose(axes=(0, 2, 1))
 
+@profile
 def dpcoo_step4(cc_da, vxdp):
     import sparse
     return vxdp.dot(sparse.COO.from_scipy_sparse(cc_da))
